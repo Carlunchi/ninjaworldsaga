6962a96bad5c7acbb34e20bb340540e77cb99ebf
Carlunchi <109547028+Carlunchi@users.noreply.github.com>
2023-11-04 03:50:57 -0300

    .

--- src/Components/LandingPage.jsx
+++ src/Components/LandingPage.jsx
@@ -4,6 +4,7 @@ import { Link } from "react-router-dom";
 const LandingPage = () => {
   return (
     <div>
+      <h1>Bienvenido</h1>
       <Link to="/home">
         <button>Home</button>
       </Link>
