7bcd8923d1341e8f4cb11a57afaa0be9aadadc36
Carlunchi <109547028+Carlunchi@users.noreply.github.com>
2023-11-03 23:50:41 -0300

    ejemplo

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import title from "/image/title1.png";
 import s from "../src/App.module.css";
+import { Link } from "react-router-dom";
 
 const App = () => {
   return (
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,23 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
+import { createHashRouter, RouterProvider } from "react-router-dom";
+import Characters from "./Components/Characters.jsx";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+const router = createHashRouter([
+  {
+    path: "/",
+    element: <App />,
+  },
+  {
+    path: "/characters",
+    element: <Characters />,
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
