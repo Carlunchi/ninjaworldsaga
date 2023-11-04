5d82c534caac9b6bfae481fde0d891c8ec1653e5
torakechi <cristianpalermotora@gmail.com>
2023-11-03 23:51:57 -0300

    Router

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,34 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+
 import './index.css'
+
+import { createHashRouter, RouterProvider } from "react-router-dom";
+
+import Home from "../src/Components/Home.jsx"
+import Characters from "../src/Components/Characters.jsx"
+import LandingPage from "../src/Components/LandingPage"
+
+const router = createHashRouter([   
+    {
+        path: "/",
+        element: <LandingPage/>
+      },
+      {
+        path: "/home",
+        element: <Home />
+      },
+      {
+        path: "/characters",
+        element: <Characters />
+      }
+])
+
+
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+   <RouterProvider router={router}/>
   </React.StrictMode>,
 )
