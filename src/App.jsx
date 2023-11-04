87020365e270701017b203d60e74adcc61880549
Carlunchi <109547028+Carlunchi@users.noreply.github.com>
2023-11-03 23:01:03 -0300

    segundo commit

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,28 @@ const App = () => {
       <div className={s.title}>
         <img src={title} alt="title" />
       </div>
+      <br />
+      <div className={s.navbar}>
+        <h2>
+          <button className={s.button}>Characters</button>
+        </h2>
+        <h2>
+          <button className={s.button}>Glossary</button>
+        </h2>
+
+        <h2>
+          <button className={s.button}>Maps</button>
+        </h2>
+        <h2>
+          <button className={s.button}>Store</button>
+        </h2>
+        <h2>
+          <button className={s.button}>Character Creation </button>
+        </h2>
+        <h2>
+          <button className={s.button}>Missions</button>
+        </h2>
+      </div>
     </div>
   );
 };
